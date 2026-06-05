ed3453ecd35e7582c37d6bdedda2fa89b3fa0351
Juan Esteban <jinteriano-2023449@kinal.edu.gt>
2026-06-05 10:47:31 -0600

    008 se agregaron cambios al Principal.java

---
+++ src/org/juaninteriano/system/Principal.java
@@ -0,0 +1,14 @@
+
+package org.juaninteriano.system;
+
+/**
+ *
+ * @Autor Juan Esteban
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo, soy Esteban");
+        System.out.println("Estoy en Kinal - 2023449");
+    }
+    
+}
